0337d54471f39a69438fe940aac6f762b5a3b9a4
Richie Staniulis <76662797+richie-stan@users.noreply.github.com>
2021-04-25 20:26:06 +0300

    Add files via upload

---
+++ Github-User/precache-manifest.33b741c4e8f09ad5e6a0b51cd6ce59f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb6cd2afccdfd02a10309ac263ea4b0",
+    "url": "/github-project/index.html"
+  },
+  {
+    "revision": "7457043bc6594e5f2508",
+    "url": "/github-project/static/css/2.ac18de59.chunk.css"
+  },
+  {
+    "revision": "4068f27feb0a2b6205c0",
+    "url": "/github-project/static/css/main.f4edeed0.chunk.css"
+  },
+  {
+    "revision": "7457043bc6594e5f2508",
+    "url": "/github-project/static/js/2.92892832.chunk.js"
+  },
+  {
+    "revision": "4068f27feb0a2b6205c0",
+    "url": "/github-project/static/js/main.630ac998.chunk.js"
+  },
+  {
+    "revision": "0306da076701c7bf4bc1",
+    "url": "/github-project/static/js/runtime-main.2fca2875.js"
+  },
+  {
+    "revision": "5cd9f30e245cbdcc11274951e439ad65",
+    "url": "/github-project/static/media/git.5cd9f30e.png"
+  }
+]);
